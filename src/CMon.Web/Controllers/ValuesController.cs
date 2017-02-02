55e27191933d1c7f5acdd4e3a5879438862a6e5b
Sergey Fadeev <fadeev@gmail.com>
2017-02-02 13:41:57 +0300

    removed unused code for metricsgraphics, c3 etc.

--- src/CMon.Web/Controllers/ValuesController.cs
+++ src/CMon.Web/Controllers/ValuesController.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Globalization;
-using System.Linq;
-using CMon.Entities;
 using CMon.Models;
 using CMon.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,59 +11,20 @@ namespace CMon.Web.Controllers
 	public class ValuesController : Controller
 	{
 		private readonly IInputValueProvider _valueProvider;
-		private readonly ConnectionStringOptions _connectionStrings;
 
-		public ValuesController(IInputValueProvider valueProvider,IOptions<ConnectionStringOptions> connectionStringOptions)
+		public ValuesController(IInputValueProvider valueProvider)
 		{
 			_valueProvider = valueProvider;
-			_connectionStrings = connectionStringOptions.Value;
 		}
 
 		// GET: api/values
-		[HttpGet("GetValues")]
+		[HttpGet]
 		public DeviceStatistic GetValues(long deviceId, int h)
 		{
 			var endDate = DateTime.UtcNow;
 			var beginDate = endDate.AddHours(-h);
 
 			return _valueProvider.GetValues(deviceId, beginDate, endDate, 1);
-		}
-
-		// GET: api/values
-		[HttpGet]
-		public IEnumerable Get(long deviceId, int h)
-		{
-			var beginDate = DateTime.UtcNow.AddHours(-h);
-
-			return new[]
-			{
-				GetValues(deviceId, 1, beginDate),
-				GetValues(deviceId, 2, beginDate),
-				GetValues(deviceId, CMon.Program.BoardTemp, beginDate)
-			};
-		}
-
-		private InputValue[] GetValues(long deviceId, short inputNum, DateTime beginDate)
-		{
-			using (var db = new DbConnection(_connectionStrings.DefaultConnection))
-			{
-				// db.Execute()
-
-				var query = from v in db.GetTable<DbInputValue>()
-							where v.DeviceId == deviceId && v.InputNum == inputNum
-								&& v.CreatedAt > beginDate
-							orderby v.CreatedAt descending
-							select v;
-
-				var values = query.ToList().Select(x => new InputValue
-				{
-					InputNum = x.InputNum,
-					Date = x.CreatedAt.ToLocalTime().ToString(CultureInfo.InvariantCulture),
-					Value = x.Value
-				}).ToArray();
-
-				return values;
-			}
 		}
 
 		// GET api/values/5
--- src/CMon.Web/wwwroot/_references.js
+++ src/CMon.Web/wwwroot/_references.js
@@ -1,7 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="js/c3.js" />
-/// <reference path="js/d3.js" />
-/// <reference path="js/metricsgraphics.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
