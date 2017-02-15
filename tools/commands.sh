02c93fc0a64a771d7a8150361b353e9e1677be04
Sergey Fadeev <fadeev@gmail.com>
2017-02-15 19:55:49 +0300

    letsencrypt certificate scripts

--- tools/commands.sh
+++ tools/commands.sh
@@ -19,3 +19,16 @@ sudo journalctl -fu dotnet-cmon.service
 sudo journalctl -fu dotnet-cmon-web.service
 
 sudo journalctl -fu dotnet-cmon-web.service --since "2016-10-18" --until "2016-10-18 04:00"
+
+# https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-16-04
+
+#IMPORTANT NOTES:
+# - Congratulations! Your certificate and chain have been saved at
+#   /etc/letsencrypt/live/ccu.montr.net/fullchain.pem. Your cert will
+#   expire on 2017-05-15. To obtain a new version of the certificate in
+#   the future, simply run Let's Encrypt again.
+
+# sudo letsencrypt certonly -a webroot --webroot-path=/var/www/html -d montr.net
+sudo letsencrypt certonly -a webroot --webroot-path=/var/www/ccu.montr.net -d ccu.montr.net
+
+# https://www.ssllabs.com/ssltest/analyze.html?d=ccu.montr.net
