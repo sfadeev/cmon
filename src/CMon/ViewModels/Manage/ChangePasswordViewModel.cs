f8cf7b26f6e02195c4bdb9606bede987c3a8acfc
Sergey Fadeev <fadeev@gmail.com>
2017-02-28 22:51:15 +0300

    added inline modals on some account/manage pages

--- src/CMon/ViewModels/Manage/ChangePasswordViewModel.cs
+++ src/CMon/ViewModels/Manage/ChangePasswordViewModel.cs
@@ -14,10 +14,5 @@ namespace CMon.ViewModels.Manage
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
     }
 }
