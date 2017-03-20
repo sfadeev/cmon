9eed8e787aad9061ae4bd7f74b12b297e3db8276
Sergey Fadeev <fadeev@gmail.com>
2017-03-21 00:28:41 +0300

    added s3 as dependency, extracted chart.js

--- src/CMon/assets/index.js
+++ src/CMon/assets/index.js
@@ -2,6 +2,3 @@ require('bootstrap-webpack!./bootstrap.config.js');
 
 require('./common');
 require('./dashboard');
-
-// require('expose?$!expose?jQuery!jquery');
-window.$ = require('jquery'); // todo: remove, required for chart
--- src/CMon/assets/sidebar.js
+++ src/CMon/assets/sidebar.js
@@ -8,8 +8,10 @@
 		var resizeSidebar = function () {
 			if ($window.width() < (768 - 20) || localStorage.getItem("sidebar-collapsed")) {
 				$body.addClass("sidebar-collapsed");
+				$sidebar.trigger("toggle");
 			} else if (!localStorage.getItem("sidebar-collapsed")) {
 				$body.removeClass("sidebar-collapsed");
+				$sidebar.trigger("toggle");
 			}
 		}
 
@@ -21,6 +23,7 @@
 				localStorage.setItem("sidebar-collapsed", true);
 			else
 				localStorage.removeItem("sidebar-collapsed");
+			$sidebar.trigger("toggle");
 		});
 
 		resizeSidebar();
