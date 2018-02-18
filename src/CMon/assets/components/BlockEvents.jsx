76e9ca84ba586aa70418ac5dc5934df89faca736
Sergey Fadeev <fadeev@gmail.com>
2018-02-18 17:58:53 +0300

    display formatted date/time in events

--- src/CMon/assets/components/BlockEvents.jsx
+++ src/CMon/assets/components/BlockEvents.jsx
@@ -40,21 +40,31 @@ class BlockEvents extends React.Component {
 							<div className="col-md-1">
 								<span className="badge">{item.externalId}</span>
 							</div>							
-							<div className="col-md-4">
-								{item.createdAt}
-							</div>
 							<div className="col-md-2">
 								<div className="media-left media-middle">
 									{item.eventType}
 								</div>
 							</div>
-							<div className="col-md-5">
+							<div className="col-md-6">
 								<code style={{wordBreak: 'break-all'}}>{JSON.stringify(item.info)}</code>
+							</div>
+							<div className="col-md-3">
+								<DateTime date={item.createdAt} />
 							</div>
 						</div>
 					);
 				})}
 			</Panel>
+		);
+	}
+}
+
+class DateTime extends React.Component {
+	render() {
+		return (
+			<time data-date={this.props.date}>
+				{new Date(this.props.date + "Z").toLocaleString("ru")}
+			</time>
 		);
 	}
 }
