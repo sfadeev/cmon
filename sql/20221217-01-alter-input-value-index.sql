e56e4adb237a8fe6f094b0bfd713050e9761dab6
Sergey Fadeev <fadeev@gmail.com>
2024-07-21 17:44:55 +0300

    removed sql scripts

--- sql/20221217-01-alter-input-value-index.sql
+++
@@ -1,4 +0,0 @@
-﻿-- CREATE INDEX ix_device_input_date ON public.input_value USING btree (device_id, input_no, created_at)
--- drop index ix_device_input_date;
-
-CREATE INDEX ix_device_input_date ON public.input_value USING btree (device_id, created_at);
